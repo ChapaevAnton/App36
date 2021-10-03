f7dd083238cb01116107966fd89bf0f1a9f276e6
Chapaev Anton <virsus2009@gmail.com>
2021-10-03 17:47:35 +0700

    37.4

--- app/src/main/java/com/w4eret1ckrtb1tch/app36/di/AppComponent.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/app36/di/AppComponent.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import com.w4eret1ckrtb1tch.app36.di.modules.AppModule
 import com.w4eret1ckrtb1tch.app36.multibindings.FormatExporter
 import com.w4eret1ckrtb1tch.app36.view.MainActivity
-import com.w4eret1ckrtb1tch.app36.viewmodel.MainViewModel
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -14,7 +13,6 @@ import javax.inject.Singleton
 @Component(modules = [AppModule::class])
 interface AppComponent : FeatureComponentInjector {
     fun inject(mainActivity: MainActivity)
-    fun inject(mainViewModel: MainViewModel)
 
     @Component.Builder
     interface Builder {
