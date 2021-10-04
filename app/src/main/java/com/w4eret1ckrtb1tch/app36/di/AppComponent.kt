90fdce129c5167dd14b8cfc3c14cc471e6c92ec6
Chapaev Anton <virsus2009@gmail.com>
2021-10-04 21:33:18 +0700

    37.5

--- app/src/main/java/com/w4eret1ckrtb1tch/app36/di/AppComponent.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/app36/di/AppComponent.kt
@@ -1,8 +1,8 @@
 package com.w4eret1ckrtb1tch.app36.di
 
 import android.content.Context
+import com.w4eret1ckrtb1tch.app36.App
 import com.w4eret1ckrtb1tch.app36.di.modules.AppModule
-import com.w4eret1ckrtb1tch.app36.multibindings.FormatExporter
 import com.w4eret1ckrtb1tch.app36.view.MainActivity
 import dagger.BindsInstance
 import dagger.Component
@@ -21,9 +21,5 @@ interface AppComponent : FeatureComponentInjector {
         @BindsInstance
         fun context(context: Context): Builder
     }
-
-    fun getFormatExporterSet(): Set<FormatExporter>
-
-    fun getFormatExporterMap(): Map<String, FormatExporter>
 
 }
--- app/src/main/java/com/w4eret1ckrtb1tch/app36/view/MainActivity.kt
+++ app/src/main/java/com/w4eret1ckrtb1tch/app36/view/MainActivity.kt
@@ -7,9 +7,9 @@ import com.w4eret1ckrtb1tch.app36.appComponent
 import com.w4eret1ckrtb1tch.app36.databinding.ActivityMainBinding
 import com.w4eret1ckrtb1tch.app36.featureComponent
 import com.w4eret1ckrtb1tch.app36.multibindings.Feature
+import com.w4eret1ckrtb1tch.app36.multibindings.FormatExporter
 import com.w4eret1ckrtb1tch.app36.multibindings.Two
 import com.w4eret1ckrtb1tch.app36.viewmodel.MainViewModel
-import dagger.Lazy
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +17,15 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var mainViewModel: MainViewModel
 
+    // FIXME: 04.10.2021 Вот это не инжектится, почему???
     @Inject
     @Two
-    lateinit var feature: Lazy<Feature>
+    lateinit var feature: Lazy<@JvmSuppressWildcards Feature>
+
+    @Inject
+    lateinit var formatSet: Set<@JvmSuppressWildcards FormatExporter>
+    @Inject
+    lateinit var formatMap: Map<@JvmSuppressWildcards String, @JvmSuppressWildcards FormatExporter>
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
 
@@ -28,12 +34,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         appComponent.inject(this)
         featureComponent.inject(this)
-
+        Log.d("TAG", "set: ${formatSet.size}")
+        Log.d("TAG", "map: ${formatMap.entries}")
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        Log.d("TAG", "set: ${appComponent.getFormatExporterSet().size}")
-        Log.d("TAG", "map: ${appComponent.getFormatExporterMap().entries}")
-        Log.d("TAG", "lazy: ${feature.get().getString()}")
 
         mainViewModel.db().observe(this) { list ->
             val text = list.toString()
